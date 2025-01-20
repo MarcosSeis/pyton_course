4f90d90ba613c054f56727c8e1089b9b39fc0ce7
Marcos Sanchez <marsanmar6291@gmail.com>
2025-01-19 19:58:00 -0600

    practica de ejecucion de creaacion de archivo y scraping

--- scraper/scrape_login/main.py
+++ scraper/scrape_login/main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 import time
 
 def get_driver():
@@ -13,10 +14,10 @@ def get_driver():
     options.add_argument('no-sandbox')
     options.add_experimental_option('excludeSwitches', ['enable-automation'])
     options.add_argument('disable-blink-features=AutomationControlled')
-    options.add_argument('--headless')
+    #options.add_argument('--headless')
     # Configura el navegador con el servicio
     driver = webdriver.Chrome(service=service, options=options)
-    driver.get('http://automated.pythonanywhere.com')
+    driver.get('http://automated.pythonanywhere.com/login/')
     return driver
 
 def clean_text(text):
@@ -26,6 +27,11 @@ def clean_text(text):
 
 def main():
     driver = get_driver()
+    driver.find_element(by="id", value="id_username").send_keys("automated")
+    time.sleep(2)
+    driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+    time.sleep(2)
+    driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
     time.sleep(2)
     element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
     return clean_text(element.text)
