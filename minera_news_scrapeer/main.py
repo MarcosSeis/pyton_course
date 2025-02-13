896b2c6448ef76df7af4e36baadf55af963491f0
Marcos Sanchez <marsanmar6291@gmail.com>
2025-02-13 17:18:14 -0600

    logica para detener ejecucion de la ia si no tiene articulos que generar

--- minera_news_scrapeer/main.py
+++ minera_news_scrapeer/main.py
@@ -3,10 +3,14 @@ from fill_articles.fill_articles import update_articles_with_content
 from ia_chat.ia_chat import process_items
 from wp_publish.wp_publish import publicar_noticias_en_wordpress
 
-
 def main():
     # Paso 1: Extraer artículos y guardarlos en JSON
     articles = get_articles()
+    
+    if not articles:  # Si la lista está vacía, termina la ejecución
+        print("No se encontraron artículos para hoy. Deteniendo la ejecución.")
+        return
+    
     save_to_json(articles)
     print("Artículos extraídos y guardados en 'articles.json'.")
 
@@ -14,13 +18,13 @@ def main():
     articles_full = update_articles_with_content()
     print("Contenido de los artículos agregado a 'articles.json'.")
 
-    #Paso 3: Usar la IA
+    # Paso 3: Usar la IA
     prompt_editor = "Eres un editor de textos de una pagina web de noticias llamada mineria news, a continuacion te voy a pasar un texto en inglés, con title, date, link y content, todo en ingles, quiero que me devuelvas un objeto con title, date y content, en español en el valor pero que las llaves las conserves igual, el value de contenent en formato con parrafos y espacios correctamente identados para la de nota para el sitio web mineria news, esto con el fin de generar una nota que sera publicada, trata de ser claro, formal pero informativo y si puedes complementar la nota con datos sobre lo que se habla hazlo, es iimportante que me regreses un objeto como te lo pedi, title, date y content."
 
     noticias = process_items(articles_full, prompt_editor)
-    print("Contenido generado con Ia listo")
+    print("Contenido generado con IA listo")
 
-    #Paso 4: Publicar de forma automatica en Wordpress
+    # Paso 4: Publicar de forma automática en Wordpress
     publicar_noticias_en_wordpress(noticias)
     print("Contenido publicado correctamente en Wordpress")
 
